121cba31ccf1cea83d4af8d4d144a05a42e6f93b
Vaibhav G <vaibhavvishnu456@gmail.com>
2025-04-09 14:59:55 +0530

    feat: add BearCounter component and integrate it into Home page

---
+++ src/components/counter/Counter.tsx
@@ -0,0 +1,17 @@
+import './Counter.css'
+
+
+const bearCounterStore = create((set) => ({)
+
+export const BearCounter = () : React.ReactNode => {
+    return(
+
+        <div className="bear-counter-root">
+            <p>X bears in my zoo!</p>
+            <button>Add bears</button>
+        </div>
+    )
+
+};
+
+export default BearCounter;
--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import NavigationBar from "../components/NavigationBar";
 import "./Home.css";
+import Container from "../components/Container";
+import BearCounter from "../components/counter/Counter";
 
 const Home = (): React.ReactNode => {
   return (
     <div className="home-root">
       <NavigationBar />
+
+      <Container>
+        <BearCounter/>
+      </Container>
     </div>
   );
 };
